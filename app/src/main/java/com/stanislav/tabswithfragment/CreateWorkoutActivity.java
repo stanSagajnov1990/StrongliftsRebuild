a56537193d9b5ea9c878768519406af4d0c8647c
Stanislav Sagajnov <stanislav.sagajnov.dev@gmail.com>
2016-06-21 08:31:42 +0200

    Create Workout Actvity created

--- app/src/main/java/com/stanislav/tabswithfragment/CreateWorkoutActivity.java
+++ app/src/main/java/com/stanislav/tabswithfragment/CreateWorkoutActivity.java
@@ -8,8 +8,11 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.UUID;
 
@@ -31,6 +34,37 @@ public class CreateWorkoutActivity extends AppCompatActivity {
 
         ActionBar ab = getSupportActionBar();
         ab.setDisplayHomeAsUpEnabled(true);
+
+        TextView editText = (TextView) findViewById(R.id.editText);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setEnabled(true);
+                v.setFocusable(true);
+                v.getId();
+            }
+        });
+
+        editText.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN:
+                        v.setBackgroundDrawable(getResources().getDrawable(R.drawable.edt_bg_selected));
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        v.setBackgroundDrawable(getResources().getDrawable(R.drawable.edt_bg_normal));
+                        break;
+//                    case MotionEvent.ACTION_CANCEL:
+//                        v.setBackgroundDrawable(getResources().getDrawable(R.drawable.edt_bg_normal));
+//                        break;
+//                    case MotionEvent.ACTION_HOVER_EXIT:
+//                        v.setBackgroundDrawable(getResources().getDrawable(R.drawable.edt_bg_normal));
+//                        break;
+                }
+                return true;
+            }
+        });
 
         workout = new Workout();
         workout.setBodyWeight("50");
@@ -54,6 +88,10 @@ public class CreateWorkoutActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_create_workout, menu);
+        return true;
+    }
+
+    public boolean onTouch(View v, MotionEvent m) {
         return true;
     }
 
