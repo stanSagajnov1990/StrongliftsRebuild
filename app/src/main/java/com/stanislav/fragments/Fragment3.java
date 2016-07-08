ca091eb6d17161ed26af9f57fcab579093ea454a
Stanislav Sagajnov <stanislav.sagajnov.dev@gmail.com>
2016-07-08 20:48:17 +0200

    created a calendar in fragment 4

--- app/src/main/java/com/stanislav/fragments/Fragment3.java
+++ app/src/main/java/com/stanislav/fragments/Fragment3.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Spinner;
 
 import com.stanislav.tabswithfragment.R;
 
@@ -15,8 +16,9 @@ public class Fragment3 extends Fragment{
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment3_layout, container, false);
+        //Spinner spinner = (Spinner) v.findViewById(R.id.spinner_nav);
+        //spinner.setEnabled(true);
 
         return v;
     }
