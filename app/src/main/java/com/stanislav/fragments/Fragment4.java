4d678031d07eedac450c333f871396a50f663b28
Stanislav Sagajnov <stanislav.sagajnov.dev@gmail.com>
2016-07-08 20:46:33 +0200

    created a calendar in fragment 4

--- app/src/main/java/com/stanislav/fragments/Fragment4.java
+++ app/src/main/java/com/stanislav/fragments/Fragment4.java
@@ -8,16 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CalendarView;
+import android.widget.Spinner;
 
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.stanislav.tabswithfragment.R;
 
-public class Fragment4 extends Fragment{
+public class Fragment4 extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
         View v = inflater.inflate(R.layout.fragment4_layout, container, false);
+
+        MaterialCalendarView calendar = (MaterialCalendarView) v.findViewById(R.id.calendarView);
+        calendar.setTopbarVisible(false);
+        calendar.setWeekDayLabels(new String[]{"S", "M", "T", "M", "T", "F", "S"});
+        calendar.setTileWidthDp(50);
+        calendar.setTileHeightDp(40);
 
         return v;
     }
