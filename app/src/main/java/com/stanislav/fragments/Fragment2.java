17ee1515cc3cf5b12b52dc6e3f17567f761a727a
Stanislav Sagajnov <stanislav.sagajnov.dev@gmail.com>
2016-07-09 11:37:24 +0200

    recyclerview implemented

--- app/src/main/java/com/stanislav/fragments/Fragment2.java
+++ app/src/main/java/com/stanislav/fragments/Fragment2.java
@@ -4,31 +4,42 @@ package com.stanislav.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.HorizontalScrollView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import com.stanislav.tabswithfragment.LiftingLab;
 import com.stanislav.tabswithfragment.R;
 import com.stanislav.tabswithfragment.Workout;
-import com.stanislav.utils.DateFormatUtils;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 public class Fragment2 extends Fragment {
+
+    private RecyclerView mWorkoutsRecyclerView;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment2_layout, container, false);
+
+        LinearLayoutManager layoutManager
+                = new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL, false);
+        mWorkoutsRecyclerView = (RecyclerView) v.findViewById(R.id.workouts_recycler_view);
+        mWorkoutsRecyclerView.setLayoutManager(layoutManager);
+
+        LiftingLab liftingLab = LiftingLab.get(getActivity());
+        List<Workout> workouts = liftingLab.getWorkouts();
+
+        WorkoutsAdapter adapter = new WorkoutsAdapter(workouts);
+        mWorkoutsRecyclerView.setAdapter(adapter);
+
+
+        /*
         final HorizontalScrollView horizontalScrollView = (HorizontalScrollView) v.findViewById(R.id.horizontalScrollView);
         LinearLayout horizontalScrollView_child = (LinearLayout) v.findViewById(R.id.horizontalScrollView_child);
 
@@ -57,7 +68,7 @@ public class Fragment2 extends Fragment {
             public void run() {
                 horizontalScrollView.fullScroll(HorizontalScrollView.FOCUS_RIGHT);
             }
-        }, 100L);
+        }, 100L); */
 
         return v;
     }
@@ -77,5 +88,53 @@ public class Fragment2 extends Fragment {
     public int getPx(int dimensionDp) {
         float density = getResources().getDisplayMetrics().density;
         return (int) (dimensionDp * density + 0.5f);
+    }
+
+    private class WorkoutsHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private Workout mWorkout;
+
+        public WorkoutsHolder(View itemView) {
+            super(itemView);
+            itemView.setOnClickListener(this);
+
+
+        }
+
+        public void bindWorkout(Workout workout){
+            mWorkout = workout;
+        }
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
+    private class WorkoutsAdapter extends RecyclerView.Adapter<WorkoutsHolder>{
+
+        private List<Workout> mWorkouts;
+
+        public WorkoutsAdapter(List<Workout> workouts) {
+            mWorkouts = workouts;
+        }
+
+        @Override
+        public WorkoutsHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater.inflate(R.layout.fragment2_column, parent, false);
+            return new WorkoutsHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(WorkoutsHolder holder, int position) {
+            Workout workout = mWorkouts.get(position);
+            holder.bindWorkout(workout);
+        }
+
+        @Override
+        public int getItemCount() {
+            return mWorkouts.size();
+        }
     }
 }
